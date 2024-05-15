6a10526e520577c8197a2067b5cdae47bd4340ac
Zwartkat <zwartkat2@gmail.com>
2024-05-15 17:27:17 +0200

    Grid gestion

--- vueApp.py
+++ vueApp.py
@@ -1,60 +1,3 @@
-#from PyQt6.QtWidgets import QWidget, QApplication, QGraphicsScene, QGraphicsView, QGraphicsRectItem,QStackedLayout, QLabel
-#from PyQt6.QtGui import QPixmap, QColor
-#import sys
-#
-#class Graphic (QGraphicsView):
-#    def __init__ (self):
-#        super().__init__()
-#        self.scene : QGraphicsScene = QGraphicsScene(self)
-#        self.setScene(self.scene)
-#        self.setStyleSheet("background-color: rgba(0, 0, 0, 0);")
-#        self.setAutoFillBackground(False)
-#        self.setContentsMargins(0, 0, 0, 0)
-#        self.scene.setBackgroundBrush(QColor(0,0,0,0))
-#        
-#        self.graphicSize : int = 20
-#        self.caseSize : int = 50
-#        
-#        for i in range(0, self.graphicSize):
-#            for j in range(0, self.graphicSize):
-#                rect : QGraphicsRectItem = QGraphicsRectItem(i*self.caseSize,j*self.caseSize,self.caseSize,self.caseSize)
-#                rect.setBrush(QColor(0,0,0,0))
-#                self.scene.addItem(rect)
-#                
-#        self.setDragMode(QGraphicsView.DragMode.ScrollHandDrag)
-#        
-#class vueGraphic(QWidget):
-#    def __init__ (self):
-#        super().__init__()
-#        
-#        self.scene = Graphic()
-#        
-#        self.mainLayout = QStackedLayout()
-#        self.label = QLabel("test")
-#        self.label.setStyleSheet("background-color: rgba(0, 0, 0, 0);")
-#        self.label.setAutoFillBackground(False)
-#        self.label.setContentsMargins(0, 0, 0, 0)
-#        self.label.setStyleSheet("background-color: rgba(0, 0, 0, 0);")
-#        #self.picture = QPixmap("./plan11.jpg")
-#        
-#        #self.pictureLabel = QLabel()
-#        #self.pictureLabel.setPixmap(self.picture)
-#        #self.pictureLabel.setStyleSheet("background-color: rgba(0, 0, 0, 0);")
-#        #
-#        
-#        self.mainLayout.addWidget(self.scene)
-#        #self.mainLayout.addWidget(self.pictureLabel)
-#        self.mainLayout.addWidget(self.label)
-#        self.setLayout(self.mainLayout)
-#        
-#        self.show()
-#        
-#if __name__ == "__main__":
-#    app = QApplication(sys.argv)
-#    vue = vueGraphic()
-#    sys.exit(app.exec())
-#    #
-
 from PyQt6.QtWidgets import QApplication, QWidget
 from PyQt6.QtCore import Qt, QPoint
 from PyQt6.QtGui import QPainter, QColor
@@ -63,16 +6,18 @@ class Grid(QWidget):
     def __init__(self):
         super().__init__()
         self.gridSize = 20
-        self.offset = QPoint(int(self.width()/2), int(self.height()/2))
+        self.offset = QPoint(0,0)
+        print(self.offset)
         self.dragging = False
         self.lastPos = QPoint()
 
     def paintEvent(self, event):
         painter = QPainter(self)
         painter.setPen(QColor("black"))
-        for x in range(-self.width(), self.width(), self.gridSize):
-            for y in range(-self.height(), self.height(), self.gridSize):
+        for x in range(0, (int)((self.width()//self.gridSize)*self.gridSize), self.gridSize):
+            for y in range(0, (int)((self.height()//self.gridSize)*self.gridSize), self.gridSize):
                 painter.drawRect(x + self.offset.x(), y + self.offset.y(), self.gridSize, self.gridSize)
+        #-(self.width()*2//self.gridSize)//4*self.gridSize,-(self.height()*2//self.gridSize)//4*self.gridSize)
 
     def mousePressEvent(self, event):
         if event.buttons() == Qt.MouseButton.LeftButton:
@@ -80,21 +25,21 @@ class Grid(QWidget):
             self.dragging = True
 
     def mouseMoveEvent(self, event):
-        
         if self.dragging:
-            if self.offset.x() < self.width() and self.offset.y() < self.height():
-                delta = event.pos() - self.lastPos
-                self.offset += delta
-                self.lastPos = event.pos()
-                print(self.offset.x(),self.offset.y())
-                self.update()
-            elif self.offset.x() <= self.width()+self.gridSize:
-                if(event.pos().x() < self.width()+ self.gridSize):
-                    x_move = event.pos().x() - self.lastPos.x()
-                    self.offset.setX(x_move)
-                    self.lastPos = event.pos()
-                    print("t")
-                    self.update()
+            if self.offset.x() < self.width() and self.offset.x()+ 2*self.width() > self.width():
+                pass
+            #if self.offset.x() < self.width() and self.offset.y() < self.height():
+            delta = event.pos() - self.lastPos
+            self.offset += delta
+            self.lastPos = event.pos()
+            print(self.offset.x(),self.offset.y())
+            self.update()
+            #elif self.offset.x() <= self.width()+self.gridSize:
+            #    if(event.pos().x() < self.width()+ self.gridSize):
+            #        x_move = event.pos().x() - self.lastPos.x()
+            #        self.offset.setX(x_move)
+            #        self.lastPos = event.pos()
+            #        self.update()
 
     def mouseReleaseEvent(self, event):
         if event.button() == Qt.MouseButton.LeftButton:
@@ -103,5 +48,6 @@ class Grid(QWidget):
 if __name__ == "__main__":
     app = QApplication([])
     gridWidget = Grid()
+    gridWidget.setMinimumSize(600,300)
     gridWidget.show()
     app.exec()
