e0cd93d267ef9a3d32a7e345f8079cb442821bcd
xMegumi <lorenzo.vdkn@gmail.com>
2024-05-15 13:07:41 +0200

    ajout des tests de bases du modele

--- modele.py
+++ modele.py
@@ -8,6 +8,12 @@ class Case:
         
     def getposition(self):
         return self.position
+    
+    def getListe(self) -> list:
+        return [self.position, self.articles, self.categorie, self.couleur, self.statut]
+    
+    def __str__(self):
+        return str([self.position, self.articles, self.categorie, self.couleur, self.statut])
         
 class Grille:
     def __init__(self, image : str, tailleGrille : tuple, verrouiller : bool):
@@ -18,36 +24,67 @@ class Grille:
 class ModelMagasin:
     def __init__(self, jsonFile : (str|None) = None) -> None:
         # attributs
-        self.__listCase : list[Grille, list[Case]] = []
+        self.__listCase = [Grille, []]
         self.__current : (tuple|None) = None
         
         # si un fichier est fourni : on charge 
         # if jsonFile: self.open(jsonFile)
         
     def ajouterCase(self, case : list):
-        caseAJoutee = Case(case[0], case[1], case[2], case[3], case[4], case[5])
+        caseAJoutee = Case(case[0], case[1], case[2], case[3], case[4])
         self.__listCase[1].append(caseAJoutee)
         
     def ajouterArticle(self, positionCase : tuple, articles : dict):
         index = 0
-        for position in self.__listCase[1].getposition():
-            if position == positionCase:
+        for position in self.__listCase[1]:
+            if position.getposition() == positionCase:
                 self.__listCase[1]
             index += 1
+    
+    def __str__(self):
+        listCases : list = []
+        if self.__listCase[1]:
+            for case in self.__listCase[1]:
+                listCases.append(case.getListe())
+        
+        return str(listCases)
+            
             
 if __name__ == '__main__':
-    # Exemple d'utilisation :
-    position = (3, 4)
-    articles = {'pomme': [5, False], 'banane': [10, True]}
-    categorie = 'Fruits'
-    couleur = 'rouge'
-    statut = True
+    # Exemple 1 : Case de légumes
+    position_vegetable = (2, 5)
+    vegetable_articles = {'carotte': [8, False], 'tomate': [12, True]}
+    vegetable_category = 'Légumes'
+    vegetable_color = 'vert'
+    vegetable_status = True
 
-    ma_case = Case(position, articles, categorie, couleur, statut)
+    vegetable_case = Case(position_vegetable, vegetable_articles, vegetable_category, vegetable_color, vegetable_status)
 
-    # Accès aux attributs de la case
-    print("Position:", ma_case.position)
-    print("Articles:", ma_case.articles)
-    print("Catégorie:", ma_case.categorie)
-    print("Couleur:", ma_case.couleur)
-    print("Statut:", ma_case.statut)
+    # Exemple 2 : Case de produits laitiers
+    position_dairy = (1, 3)
+    dairy_articles = {'lait': [6, False], 'fromage': [15, True]}
+    dairy_category = 'Produits laitiers'
+    dairy_color = 'blanc'
+    dairy_status = False
+
+    dairy_case = Case(position_dairy, dairy_articles, dairy_category, dairy_color, dairy_status)
+
+    # Exemple 3 : Case de produits d'hygiène
+    position_hygiene = (0, 0)
+    hygiene_articles = {'savon': [10, True], 'dentifrice': [8, True]}
+    hygiene_category = 'Produits d\'hygiène'
+    hygiene_color = 'bleu'
+    hygiene_status = False
+
+    ma_case = Case(position_vegetable, vegetable_articles, vegetable_category, vegetable_color, vegetable_status)
+    list_case = ma_case.getListe()
+    print(list_case)
+    magasin = ModelMagasin()
+    
+    magasin.ajouterCase(list_case)
+    magasin.ajouterArticle(position_hygiene, hygiene_articles)
+    
+    print("test des classes : \n")
+    print(ma_case)
+    print(magasin)
+
