959e96ad92600650487d6ebe7889dad522a5b197
Hildragul <theomorel642@gmail.com>
2024-05-15 17:23:25 +0200

    modificationArticle, changerVer et quantité

--- modele.py
+++ modele.py
@@ -1,20 +1,53 @@
 class Case:
-    def __init__(self, position, articles, categorie, couleur, statut):
+    def __init__(self, position : tuple, articles: dict, categorie : str, couleur : str, statut : str):
         self.position = position
         self.articles = articles
         self.categorie = categorie
         self.couleur = couleur
         self.statut = statut
+
         
-    def getposition(self):
+    # getters
+    def getPosition(self) -> tuple:
         return self.position
+    
+    def getArticles(self) -> dict:
+        return self.articles
+    
+    def getCategorie(self) -> str:
+        return self.categorie
+    
+    def getCouleur(self) -> str:
+        return self.couleur
+    
+    def getStatut(self) -> str:
+        return self.statut
     
     def getListe(self) -> list:
         return [self.position, self.articles, self.categorie, self.couleur, self.statut]
     
+    
+    # setters
+    def setposition(self, position : tuple):
+        self.position = position
+        
+    def setArticles(self, articles : dict):
+        self.articles = articles
+        
+    def setCategorie(self, categorie : str):
+        self.categorie = categorie
+        
+    def setCouleur(self, couleur : str):
+        self.couleur = couleur
+        
+    def setStatut(self, statut : str):
+        self.statut = statut
+    
+    
     def __str__(self):
         return str([self.position, self.articles, self.categorie, self.couleur, self.statut])
-        
+                        
+                
 class Grille:
     def __init__(self, image : str, tailleGrille : tuple, verrouiller : bool):
         self.image = image
@@ -37,7 +70,7 @@ class ModelMagasin:
     def ajouterArticle(self, positionCase : tuple, articles : dict):
         index = 0
         for position in self.__listCase[1]:
-            if position.getposition() == positionCase:
+            if position.getPosition() == positionCase:
                 self.__listCase[1]
             index += 1
     
@@ -48,7 +81,41 @@ class ModelMagasin:
                 listCases.append(case.getListe())
         
         return str(listCases)
+    
+       
+    def changerQuant(self, position : tuple, nomArticle : str, quantite: int) -> str | None:
+        for case in self.__listCase[1]: 
+            if case.getPosition() == position:
+                if (case.getArticles()[nomArticle][1] == True ):
+                    return ("Quantité verrouillée")
+                elif (quantite > 0):
+                    case.getArticles()[nomArticle][0] = quantite
+                else : 
+                    return ("Quantité invalide")
+            else:
+                return ("Article non trouvé")
+        
+    def changerVerrouillage(self, position : tuple, nomArticle : str) -> None:
+        for case in self.__listCase[1]:
+            if case.getPosition() == position:
+                if (case.getArticles()[nomArticle][1] == False ):
+                    case.getArticles()[nomArticle][1] = True
+                else : 
+                    case.getArticles()[nomArticle][1] = False
+            else:
+                return ("Article non trouvé")
             
+    def modifierArticle(self, positionCase : tuple, nomArticle : str, quantite: (int|bool)) -> None:
+            for case in self.__listCase[1]:
+                if case.getPosition() == positionCase:
+                    if isinstance(quantite, bool):
+                        self.changerVerrouillage(positionCase, nomArticle)
+                    else:
+                        self.changerQuant(positionCase, nomArticle, quantite)  
+                else:
+                    return ("Article non trouvé")
+ 
+             
             
 if __name__ == '__main__':
     # Exemple 1 : Case de légumes
@@ -85,6 +152,14 @@ if __name__ == '__main__':
     magasin.ajouterArticle(position_hygiene, hygiene_articles)
     
     print("test des classes : \n")
+    print(ma_case)
+    magasin.modifierArticle((2,5), 'carotte', 18)
+
+    magasin.modifierArticle((2,5), 'carotte', True)
+    
+    print('changement verrouillage : \n')
+    print(ma_case)
+    magasin.modifierArticle((2,5), 'carotte', 10)
     print(ma_case)
     print(magasin)
 
