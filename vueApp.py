685456a686a61748cf999fad998f60ab40df8f4a
Zwartkat <zwartkat2@gmail.com>
2024-05-16 15:12:04 +0200

    Grid graphicscene generation (good version)

--- vueApp.py
+++ vueApp.py
@@ -1,48 +1,66 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QGraphicsView, QGraphicsScene, QGraphicsRectItem
-from PyQt6.QtGui import QColor, QMouseEvent, QWheelEvent
-from PyQt6.QtCore import Qt, QPointF
+from PyQt6.QtWidgets import QApplication, QMainWindow, QGraphicsView, QGraphicsScene, QGraphicsLineItem
+from PyQt6.QtCore import Qt, QPoint
 
-class GraphicsView(QGraphicsView):
-    def __init__(self):
-        super().__init__()
-        self.setDragMode(QGraphicsView.DragMode.ScrollHandDrag)
-        self.setTransformationAnchor(QGraphicsView.ViewportAnchor.AnchorUnderMouse)
-        
-        self.setSceneRect(-500, -500, 1000, 1000)  # Initial scene rectangle
-        self.centerOn(0, 0)  # Center the view on the origin
-
-        self._zoom = 0
-        self.zoom_factor = 1.25
-
-class MainWindow(QGraphicsView):
-    def __init__(self):
-        super().__init__()
-
+class Grid(QGraphicsView):
+    def __init__(self, parent=None):
+        super(Grid, self).__init__(parent)
         self.scene = QGraphicsScene(self)
         self.setScene(self.scene)
+        
+        self.offset = QPoint(0,0)
+        self.lastPos = QPoint(0,0)
+        self.dragging = False
+        
+        self.gridSize = 8
+        self.gridStep = 20
+        self.drawGrid()
 
-        # Add some items to the scene
-        self.scene.addRect(-100, -100, 200, 200, pen=QColor("black"))
-        self.scene.addRect(-200, -200, 400, 400, pen=QColor("red"))
+    def drawGrid(self):
+        self.scene.clear()
+        for i in range(-self.gridSize, self.gridSize+1):
+            self.scene.addItem(QGraphicsLineItem(i*self.gridStep + self.offset.x(), -self.gridSize*self.gridStep  + self.offset.y(), i*self.gridStep  + self.offset.x(), self.gridSize*self.gridStep  + self.offset.y()))
+            self.scene.addItem(QGraphicsLineItem(-self.gridSize*self.gridStep  + self.offset.x(), i*self.gridStep  + self.offset.y(), self.gridSize*self.gridStep  + self.offset.x(), i*self.gridStep  + self.offset.y()))
 
-        # Enable dragging the scene with the mouse
-        self.setDragMode(QGraphicsView.DragMode.ScrollHandDrag)
-        self.setSceneRect(-300, -300, 600, 600)
+    def mousePressEvent(self, event):
+        if event.buttons() == Qt.MouseButton.LeftButton:
+            self.lastPos = event.pos()
+            self.dragging = True
+
+    def mouseMoveEvent(self, event):
+        if self.dragging:
+            if self.offset.x() < self.width() and self.offset.x()+ 2*self.width() > self.width():
+                pass
+            #if self.offset.x() < self.width() and self.offset.y() < self.height():
+            delta = event.pos() - self.lastPos
+            self.offset += delta
+            self.lastPos = event.pos()
+            print(self.offset.x(),self.offset.y())
+            self.drawGrid()
+            #elif self.offset.x() <= self.width()+self.gridSize:
+            #    if(event.pos().x() < self.width()+ self.gridSize):
+            #        x_move = event.pos().x() - self.lastPos.x()
+            #        self.offset.setX(x_move)
+            #        self.lastPos = event.pos()
+            #        self.update()
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.MouseButton.LeftButton:
+            self.dragging = False
+        
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.setGeometry(100, 100, 600, 400)
+        self.setWindowTitle("Grille déplaçable avec PyQt6")
+
+        self.grid = Grid()
+        self.setCentralWidget(self.grid)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-
-    view = GraphicsView()
-    scene = QGraphicsScene()
-    view.setScene(scene)
-
-    # Add a few rectangles to the scene
-    for i in range(5):
-        for j in range(5):
-            rect = QGraphicsRectItem(i * 100, j * 100, 80, 80)
-            rect.setBrush(QColor("lightblue"))
-            scene.addItem(rect)
-
-    view.show()
+    window = MainWindow()
+    window.show()
     sys.exit(app.exec())
