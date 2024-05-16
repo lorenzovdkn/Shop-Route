c85e50788dd5abe3d6bd96a7cdbe78f416dfd9ad
Zwartkat <zwartkat2@gmail.com>
2024-05-16 15:09:45 +0200

    Grid graphicscene generation

--- vueApp.py
+++ vueApp.py
@@ -1,53 +1,48 @@
-from PyQt6.QtWidgets import QApplication, QWidget
-from PyQt6.QtCore import Qt, QPoint
-from PyQt6.QtGui import QPainter, QColor
+import sys
+from PyQt6.QtWidgets import QApplication, QGraphicsView, QGraphicsScene, QGraphicsRectItem
+from PyQt6.QtGui import QColor, QMouseEvent, QWheelEvent
+from PyQt6.QtCore import Qt, QPointF
 
-class Grid(QWidget):
+class GraphicsView(QGraphicsView):
     def __init__(self):
         super().__init__()
-        self.gridSize = 20
-        self.offset = QPoint(0,0)
-        print(self.offset)
-        self.dragging = False
-        self.lastPos = QPoint()
+        self.setDragMode(QGraphicsView.DragMode.ScrollHandDrag)
+        self.setTransformationAnchor(QGraphicsView.ViewportAnchor.AnchorUnderMouse)
+        
+        self.setSceneRect(-500, -500, 1000, 1000)  # Initial scene rectangle
+        self.centerOn(0, 0)  # Center the view on the origin
 
-    def paintEvent(self, event):
-        painter = QPainter(self)
-        painter.setPen(QColor("black"))
-        for x in range(0, (int)((self.width()//self.gridSize)*self.gridSize), self.gridSize):
-            for y in range(0, (int)((self.height()//self.gridSize)*self.gridSize), self.gridSize):
-                painter.drawRect(x + self.offset.x(), y + self.offset.y(), self.gridSize, self.gridSize)
-        #-(self.width()*2//self.gridSize)//4*self.gridSize,-(self.height()*2//self.gridSize)//4*self.gridSize)
+        self._zoom = 0
+        self.zoom_factor = 1.25
 
-    def mousePressEvent(self, event):
-        if event.buttons() == Qt.MouseButton.LeftButton:
-            self.lastPos = event.pos()
-            self.dragging = True
+class MainWindow(QGraphicsView):
+    def __init__(self):
+        super().__init__()
 
-    def mouseMoveEvent(self, event):
-        if self.dragging:
-            if self.offset.x() < self.width() and self.offset.x()+ 2*self.width() > self.width():
-                pass
-            #if self.offset.x() < self.width() and self.offset.y() < self.height():
-            delta = event.pos() - self.lastPos
-            self.offset += delta
-            self.lastPos = event.pos()
-            print(self.offset.x(),self.offset.y())
-            self.update()
-            #elif self.offset.x() <= self.width()+self.gridSize:
-            #    if(event.pos().x() < self.width()+ self.gridSize):
-            #        x_move = event.pos().x() - self.lastPos.x()
-            #        self.offset.setX(x_move)
-            #        self.lastPos = event.pos()
-            #        self.update()
+        self.scene = QGraphicsScene(self)
+        self.setScene(self.scene)
 
-    def mouseReleaseEvent(self, event):
-        if event.button() == Qt.MouseButton.LeftButton:
-            self.dragging = False
-            
+        # Add some items to the scene
+        self.scene.addRect(-100, -100, 200, 200, pen=QColor("black"))
+        self.scene.addRect(-200, -200, 400, 400, pen=QColor("red"))
+
+        # Enable dragging the scene with the mouse
+        self.setDragMode(QGraphicsView.DragMode.ScrollHandDrag)
+        self.setSceneRect(-300, -300, 600, 600)
+
 if __name__ == "__main__":
-    app = QApplication([])
-    gridWidget = Grid()
-    gridWidget.setMinimumSize(600,300)
-    gridWidget.show()
-    app.exec()
+    app = QApplication(sys.argv)
+
+    view = GraphicsView()
+    scene = QGraphicsScene()
+    view.setScene(scene)
+
+    # Add a few rectangles to the scene
+    for i in range(5):
+        for j in range(5):
+            rect = QGraphicsRectItem(i * 100, j * 100, 80, 80)
+            rect.setBrush(QColor("lightblue"))
+            scene.addItem(rect)
+
+    view.show()
+    sys.exit(app.exec())
