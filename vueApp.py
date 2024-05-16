508e210a44f8a805bccd4dd96fb88e465ee7bff0
Zwartkat <zwartkat2@gmail.com>
2024-05-16 15:55:43 +0200

    Grid generation clean

--- vueApp.py
+++ vueApp.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QGraphicsView, QGraphicsScene, QGraphicsLineItem
+from PyQt6.QtWidgets import QApplication, QMainWindow, QGraphicsView, QGraphicsScene, QGraphicsRectItem
 from PyQt6.QtCore import Qt, QPoint
 
 class Grid(QGraphicsView):
@@ -8,42 +8,39 @@ class Grid(QGraphicsView):
         self.scene = QGraphicsScene(self)
         self.setScene(self.scene)
         
-        self.offset = QPoint(0,0)
+        self.gridSize = 100
+        self.gridStep = 20
+
+        self.offset = QPoint(int(-self.gridStep * (self.gridSize/2)) , (int)(-self.gridStep * (self.gridSize/2)))
         self.lastPos = QPoint(0,0)
         self.dragging = False
         
-        self.gridSize = 8
-        self.gridStep = 20
         self.drawGrid()
 
+
+    # Draw the grid
     def drawGrid(self):
         self.scene.clear()
-        for i in range(-self.gridSize, self.gridSize+1):
-            self.scene.addItem(QGraphicsLineItem(i*self.gridStep + self.offset.x(), -self.gridSize*self.gridStep  + self.offset.y(), i*self.gridStep  + self.offset.x(), self.gridSize*self.gridStep  + self.offset.y()))
-            self.scene.addItem(QGraphicsLineItem(-self.gridSize*self.gridStep  + self.offset.x(), i*self.gridStep  + self.offset.y(), self.gridSize*self.gridStep  + self.offset.x(), i*self.gridStep  + self.offset.y()))
-
+        for x in range(0, self.gridSize):
+            for y in range(0, self.gridSize):
+                self.scene.addItem(QGraphicsRectItem(x*self.gridStep + self.offset.x(), y*self.gridStep + self.offset.y(), self.gridStep, self.gridStep))
+     
+     # Enable the click move event   
     def mousePressEvent(self, event):
         if event.buttons() == Qt.MouseButton.LeftButton:
             self.lastPos = event.pos()
             self.dragging = True
 
+    # Move the grid if a click is detected
     def mouseMoveEvent(self, event):
         if self.dragging:
-            if self.offset.x() < self.width() and self.offset.x()+ 2*self.width() > self.width():
-                pass
-            #if self.offset.x() < self.width() and self.offset.y() < self.height():
             delta = event.pos() - self.lastPos
             self.offset += delta
             self.lastPos = event.pos()
             print(self.offset.x(),self.offset.y())
             self.drawGrid()
-            #elif self.offset.x() <= self.width()+self.gridSize:
-            #    if(event.pos().x() < self.width()+ self.gridSize):
-            #        x_move = event.pos().x() - self.lastPos.x()
-            #        self.offset.setX(x_move)
-            #        self.lastPos = event.pos()
-            #        self.update()
 
+    # Disable the move click event
     def mouseReleaseEvent(self, event):
         if event.button() == Qt.MouseButton.LeftButton:
             self.dragging = False
@@ -51,8 +48,7 @@ class Grid(QGraphicsView):
 
 class MainWindow(QMainWindow):
     def __init__(self):
-        super(MainWindow, self).__init__()
-
+        super(MainWindow,self).__init__()
         self.setGeometry(100, 100, 600, 400)
         self.setWindowTitle("Grille déplaçable avec PyQt6")
 
@@ -62,5 +58,5 @@ class MainWindow(QMainWindow):
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
-    window.show()
+    window.showMaximized()
     sys.exit(app.exec())
