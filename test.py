5ca61dbcba61ad1425d0ba0c15366db6eef59296
Hildragul <theomorel642@gmail.com>
2024-05-23 14:59:35 +0200

    version avec QListWidget

---
+++ test.py
@@ -0,0 +1,316 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QHBoxLayout, QVBoxLayout, QGraphicsView, QGraphicsScene, QGraphicsRectItem, QGraphicsPixmapItem, QFileDialog, QComboBox, QLabel, QListWidget, QInputDialog, QPushButton, QLineEdit, QMessageBox
+from PyQt6.QtCore import Qt, QPoint, pyqtSignal
+from PyQt6.QtGui import QPixmap, QFont
+
+class Grid(QGraphicsView):
+    
+    positionSignal : pyqtSignal = pyqtSignal(QPoint)
+    lockedSignal : pyqtSignal = pyqtSignal(bool)
+    sizeSignal : pyqtSignal = pyqtSignal(int,int)
+    stepSignal : pyqtSignal = pyqtSignal(int)
+    offsetSignal : pyqtSignal = pyqtSignal(tuple)
+    
+    def __init__(self, parent=None):
+        super(Grid, self).__init__(parent)
+        
+        self.scene : QGraphicsScene = QGraphicsScene(self)
+        self.setScene(self.scene)
+        
+        self.gridStep : int = 20
+        self.width : int =  50
+        self.height : int = 50
+        self.offset : QPoint = QPoint(0,0)
+        self.lastPos : QPoint = QPoint(0,0)
+        self.dragging : bool = False
+        self.locked : bool = True
+        self.picture : str = None
+        
+        self.drawGrid()
+        self.sceneWidth = self.scene.width()
+        self.sceneHeight = self.scene.height()
+
+    def getGridSize(self):
+        return (self.width,self.height)
+    
+    def getGridStep(self):
+        return self.gridStep
+        
+    def isLocked(self):
+        return self.locked
+
+    def setPicture(self, picture : str):
+        self.picture = picture
+    # Draw the grid
+    def drawGrid(self, width: int = None, height: int = None):
+        
+        if(width is None):
+            width = self.width
+        else:
+            self.width = width
+        if(height is None):
+            height = self.height
+        else:
+            self.height = height
+            
+        self.scene.clear()
+        
+        if(self.picture != None):
+            pixmap = QPixmap(self.picture)
+            self.image_item = QGraphicsPixmapItem(pixmap)
+            self.scene.addItem(self.image_item)
+        
+        self.offsetSignal.emit((self.offset.x(), self.offset.y()))
+        
+        for x in range(-1, width):
+            for y in range(-1, height):
+                rect : QGraphicsRectItem = QGraphicsRectItem(x*self.gridStep + self.offset.x(), y*self.gridStep + self.offset.y(), self.gridStep, self.gridStep)
+                self.scene.addItem(rect)
+                
+     # Manage the click event
+    def mousePressEvent(self, event):
+        if event.buttons() == Qt.MouseButton.LeftButton:
+            if self.locked:
+                # Get the case who the user clicked
+                scenePos : QPoint = self.mapToScene(event.pos())
+                posX : int = (int) ((scenePos.x() - self.offset.x()) // self.gridStep + 1)
+                posY : int = (int) ((scenePos.y() - self.offset.y()) // self.gridStep + 1)
+                print(posX, posY)
+                self.positionSignal.emit(QPoint(posX, posY))
+            else:
+                # Enable grid movement
+                self.lastPos = event.pos()
+                self.dragging = True
+
+    # Move the grid if a click is detected
+    def mouseMoveEvent(self, event):
+        if self.dragging and not self.locked:
+            delta = event.pos() - self.lastPos
+            self.offset += delta
+            if self.offset.x() <= -self.sceneWidth//10:
+                self.offset.setX(0)
+                self.dragging = False
+            if self.offset.x() + (self.width * self.gridStep) > self.sceneWidth + self.sceneWidth//10:
+                self.offset.setX((int) (self.sceneWidth - (self.width * self.gridStep)))
+                self.dragging = False
+            if self.offset.y() <= -self.sceneHeight//10:
+                self.offset.setY(0)
+                self.dragging = False
+            if self.offset.y() + (self.height * self.gridStep) > self.sceneHeight + self.sceneHeight//10:
+                self.offset.setY((int) (self.sceneHeight - (self.height * self.gridStep)))
+                self.dragging = False
+            
+            self.lastPos = event.pos()
+            self.drawGrid()
+
+    # Disable the move click event
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.MouseButton.LeftButton:
+            self.dragging = False
+    
+    # Scroll event
+    def wheelEvent(self, event):
+        # Reduce the size of the grid
+        if event.angleDelta().y() < 0 and not self.locked:
+            if(self.gridStep > 10):
+                event.ignore()
+                self.gridStep = self.gridStep - 0.25
+                self.drawGrid()
+        # Increase the size of the grid
+        elif event.angleDelta().y() > 0 and not self.locked:
+            if(self.gridStep < 50):
+                event.ignore()
+                self.gridStep = self.gridStep + 0.25
+                self.drawGrid()
+        
+class Case(QWidget):
+    
+    def __init__(self):
+        
+        super().__init__()
+        self.layout1 = QVBoxLayout()
+        self.layout2 = QHBoxLayout()
+        self.layout3 = QHBoxLayout()
+        self.layout4 = QHBoxLayout()
+
+        self.setLayout(self.layout1)
+        self.resize(800, 600) 
+        
+        self.titre = QLabel("Mode Edition de plan")
+        self.titre_font = QFont()
+        self.titre_font.setPointSize(30)
+        self.titre.setFont(self.titre_font)
+
+        self.case = QLabel("Case")
+        self.case_font = QFont()
+        self.case_font.setPointSize(20)
+        self.case.setFont(self.case_font)
+
+        self.type_case_label = QLabel("Type de case:")
+        self.type_case_combo = QComboBox()
+        self.type_case_combo.addItems(["publique", "privé"])
+        
+        self.category_label = QLabel("Catégorie de la case:")
+        self.category_combo = QComboBox()
+        
+        position = (0, 0)
+        self.case_number_label = QLabel("Numéro de la case:")
+        self.case_number = QLineEdit(f"{position[0]}, {position[1]}")
+        self.case_number.setReadOnly(True)
+        
+        # layouts
+        self.layout1.addWidget(self.titre)
+        self.layout1.addWidget(self.case)
+        self.layout1.addLayout(self.layout2)
+        self.layout1.addLayout(self.layout3)
+        self.layout1.addLayout(self.layout4)
+        self.layout2.addWidget(self.type_case_label)
+        self.layout2.addWidget(self.type_case_combo)
+        self.layout3.addWidget(self.category_label)
+        self.layout3.addWidget(self.category_combo)
+        self.layout4.addWidget(self.case_number_label)
+        self.layout4.addSpacing(61)
+        self.layout4.addWidget(self.case_number)
+        
+        # Alignement des layouts
+        self.layout1.setAlignment(Qt.AlignmentFlag.AlignTop)
+        
+        # signaux
+        #self.category_combo.currentIndexChanged.connect(self.signalChangedCategory.emit)
+        
+    # Set the display of the current case
+    def setCase(self, position : QPoint):
+        self.case_number.setText(f"{position.x()}, {position.y()}")
+    
+    def updateProductCategory(self, list_article: list):
+        self.category_combo.addItems(list_article)
+    
+    def currentCategory(self):
+        return self.category_combo.currentText()
+    
+    def currentCase(self):
+        pass
+
+
+class Contenu(QWidget):
+    
+    signalAddProduct = pyqtSignal()
+    signalProduct = pyqtSignal(dict)
+    signalDeleteProduct = pyqtSignal(str)
+    signalEditProduct = pyqtSignal(list) # [nomDuProduit, Quantité]
+    signalProductClick = pyqtSignal(dict)
+    
+    def __init__(self):
+        super().__init__()
+        self.layout1 = QVBoxLayout()
+        self.setLayout(self.layout1)
+        self.resize(800, 600) 
+        self.case = None
+        
+        contenu = QLabel("Contenu :")
+        self.productList = QListWidget()
+        self.addButton = QPushButton('Ajouter un produit')
+        self.removeButton = QPushButton('Supprimer un produit')
+        
+        self.layout1.addWidget(contenu)
+        self.layout1.addWidget(self.productList)
+        self.layout1.addWidget(self.addButton)
+        self.layout1.addWidget(self.removeButton)
+        
+        self.productList.itemClicked.connect(self.productClicked)
+        self.productList.itemDoubleClicked.connect(self.editProductDoubleClicked)
+        self.addButton.clicked.connect(self.signalAddProduct.emit)
+        self.removeButton.clicked.connect(self.removeProductClicked)
+      
+    def setCase(self, case : QPoint):
+        self.case = (case.x(), case.y()); 
+    
+    def getCase(self):
+        return self.case;
+        
+    # Permet d'ajouter un produit dans la liste si une catégorie est choisie
+    def addProduct(self, product_list_import : list, current_category : str):
+        if current_category == 'aucune':
+            QMessageBox.warning(self, "Erreur", "Veuillez sélectionner une catégorie de case pour ajouter un produit.")
+            return 
+        
+        product_list = product_list_import
+        product, ok = QInputDialog.getItem(self, 'Ajouter un produit', 'Sélectionnez un produit:',product_list,0,False)        
+        if ok and product:
+            quantity, ok = QInputDialog.getInt(self, 'Ajouter un produit', 'Quantité:', 1, 1)
+            if ok:
+                item_text = f"{product} - Quantité: {quantity}"
+                self.signalProduct.emit({product: [quantity, False]})
+                
+    # Permet de mettre à jour l'affichage de la liste des produits       
+    def updateArticle(self, articles : dict | None) :
+        self.productList.clear()
+        if articles != None :
+            for key, value in articles.items():
+                item_text = f"{key} - Quantité : {value[0]}"
+                self.productList.addItem(item_text)
+    
+    # Permet de supprimer un élement de la liste
+    def removeProductClicked(self):
+        selected_items = self.productList.selectedItems()
+        
+        if selected_items:
+            item = selected_items[0]
+            nameSelection = item.text()
+            parts = nameSelection.split(' - ')
+            nameArticle = parts[0]
+            print(nameArticle)
+            self.signalDeleteProduct.emit(nameArticle)
+
+    def productClicked(self, item):
+        item_text = item.text()
+        product, quantity = item_text.split(" - Quantité : ")
+        quantity = int(quantity)
+        print({product : [quantity, False]})
+        self.signalProductClick.emit({product : [quantity, False]})
+
+        
+    def editProductDoubleClicked(self, item):
+        item_text = item.text()
+        product, quantity = item_text.split(" - Quantité : ")
+        
+        new_quantity, ok = QInputDialog.getInt(self, 'Modifier un produit', f'Nouvelle quantité pour {product}:', int(quantity), 1)
+
+        if ok:
+            self.signalEditProduct.emit([product, new_quantity])
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        
+        menu_bar = self.menuBar()
+        menu_fichier = menu_bar.addMenu("Fichier")
+        
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
+        self.mainLayout = QHBoxLayout(self.central_widget)
+        self.leftLayout = QVBoxLayout()
+        self.mainLayout.addLayout(self.leftLayout)
+        
+        
+        # Widget creation
+        self.case_widget = Case()
+        self.contenu_widget = Contenu()
+        self.grid = Grid()
+
+        self.leftLayout.addWidget(self.case_widget)
+        self.leftLayout.addWidget(self.contenu_widget)
+        
+        self.mainLayout.addWidget(self.grid)
+        
+        # Signals definition
+        self.grid.positionSignal.connect(self.case_widget.setCase)
+        self.grid.positionSignal.connect(self.contenu_widget.setCase)
+        
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
--- vueApp.py
+++ vueApp.py
@@ -1,56 +1,243 @@
-from PyQt6.QtWidgets import QWidget, QApplication, QGraphicsScene, QGraphicsView, QGraphicsRectItem,QStackedLayout, QLabel
-from PyQt6.QtGui import QPixmap, QColor
 import sys
+import time
+from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QWidget, QPushButton, QDockWidget, QFileDialog, QListWidget, QGraphicsView, QGraphicsScene, QGraphicsRectItem, QGraphicsPixmapItem, QListWidgetItem
+from PyQt6.QtCore import Qt, QPoint, pyqtSignal
+from PyQt6.QtGui import QPixmap, QColor
 
-class Graphic (QGraphicsView):
-    def __init__ (self):
-        super().__init__()
-        self.scene : QGraphicsScene = QGraphicsScene(self)
+class Grid(QGraphicsView):
+    positionSignal = pyqtSignal(QPoint)
+    lockedSignal = pyqtSignal(bool)
+    sizeSignal = pyqtSignal(int, int)
+    stepSignal = pyqtSignal(int)
+    offsetSignal = pyqtSignal(tuple)
+
+    def __init__(self, parent=None):
+        super(Grid, self).__init__(parent)
+        
+        self.scene = QGraphicsScene(self)
         self.setScene(self.scene)
-        self.setStyleSheet("background-color: rgba(0, 0, 0, 0);")
-        self.setAutoFillBackground(False)
-        self.setContentsMargins(0, 0, 0, 0)
-        self.scene.setBackgroundBrush(QColor(0,0,0,0))
         
-        self.graphicSize : int = 20
-        self.caseSize : int = 50
+        self.gridStep = 20
+        self.width = 50
+        self.height = 50
+        self.offset = QPoint(0, 0)
+        self.lastPos = QPoint(0, 0)
+        self.dragging = False
+        self.locked = True
+        self.picture = "./plan11.jpg"
+        self.grid = {}
         
-        for i in range(0, self.graphicSize):
-            for j in range(0, self.graphicSize):
-                rect : QGraphicsRectItem = QGraphicsRectItem(i*self.caseSize,j*self.caseSize,self.caseSize,self.caseSize)
-                rect.setBrush(QColor(0,0,0,0))
+        self.drawGrid()
+        self.sceneWidth = self.scene.width()
+        self.sceneHeight = self.scene.height()
+
+    def getGridSize(self):
+        return (self.width, self.height)
+    
+    def getGridStep(self):
+        return self.gridStep
+        
+    def isLocked(self):
+        return self.locked
+
+    def setPicture(self, picture):
+        self.picture = picture
+    
+    def setGrid(self, grid):
+        self.grid = grid
+        
+    def drawGrid(self, width=None, height=None, step=None, position_dict=None):
+        if width is None:
+            width = self.width
+        else:
+            self.width = width
+        if height is None:
+            height = self.height
+        else:
+            self.height = height
+        if step is None:
+            step = self.gridStep
+        if position_dict is None:
+            position_dict = self.grid
+            
+        self.scene.clear()
+        
+        if self.picture is not None:
+            pixmap = QPixmap(self.picture)
+            self.image_item = QGraphicsPixmapItem(pixmap)
+            self.scene.addItem(self.image_item)
+        
+        position_dict = {(1, 3): "red", (2, 5): "blue"}
+        
+        for x in range(-1, width):
+            for y in range(-1, height):
+                rect = QGraphicsRectItem(x * step + self.offset.x(), y * step + self.offset.y(), step, step)
+                if (x, y) in [position for position in position_dict.keys()]:
+                    color = QColor(position_dict.get((x, y)))
+                    color.setAlpha(100)
+                    rect.setBrush(color)
                 self.scene.addItem(rect)
                 
-        self.setDragMode(QGraphicsView.DragMode.ScrollHandDrag)
-        
-class vueGraphic(QWidget):
-    def __init__ (self):
+    def mousePressEvent(self, event):
+        if event.buttons() == Qt.MouseButton.LeftButton:
+            if self.locked:
+                scenePos = self.mapToScene(event.pos())
+                posX = int((scenePos.x() - self.offset.x()) // self.gridStep + 1)
+                posY = int((scenePos.y() - self.offset.y()) // self.gridStep + 1)
+                print(posX, posY)
+                self.positionSignal.emit(QPoint(posX, posY))
+            else:
+                self.lastPos = event.pos()
+                self.dragging = True
+
+    def mouseMoveEvent(self, event):
+        if self.dragging and not self.locked:
+            delta = event.pos() - self.lastPos
+            self.offset += delta
+            if self.offset.x() <= -self.sceneWidth // 10:
+                self.offset.setX(0)
+                self.dragging = False
+            if self.offset.x() + (self.width * self.gridStep) > self.sceneWidth + self.sceneWidth // 10:
+                self.offset.setX(int(self.sceneWidth - (self.width * self.gridStep)))
+                self.dragging = False
+            if self.offset.y() <= -self.sceneHeight // 10:
+                self.offset.setY(0)
+                self.dragging = False
+            if self.offset.y() + (self.height * self.gridStep) > self.sceneHeight + self.sceneHeight // 10:
+                self.offset.setY(int(self.sceneHeight - (self.height * self.gridStep)))
+                self.dragging = False
+            
+            self.lastPos = event.pos()
+            self.drawGrid()
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.MouseButton.LeftButton:
+            self.dragging = False
+    
+    def wheelEvent(self, event):
+        if event.angleDelta().y() < 0 and not self.locked:
+            if self.gridStep > 10:
+                event.ignore()
+                self.gridStep -= 1
+                self.drawGrid()
+                time.sleep(0.01)
+        elif event.angleDelta().y() > 0 and not self.locked:
+            if self.gridStep < 50:
+                event.ignore()
+                self.gridStep += 1
+                self.drawGrid()
+                time.sleep(0.01)
+    
+    def lockGrid(self):
+        if not self.locked:
+            self.locked = True
+            self.sizeSignal.emit(self.width, self.height)
+            self.stepSignal.emit(self.gridStep)
+            self.offsetSignal.emit((self.offset.x(), self.offset.y()))
+            
+
+
+class VueProjet(QMainWindow):
+    ajoutClicked = pyqtSignal()
+    supprimerClicked = pyqtSignal()
+    analyseClicked = pyqtSignal()
+    fnameOpen = pyqtSignal(str)
+
+    def __init__(self):
         super().__init__()
         
-        self.scene = Graphic()
+        self.setWindowTitle('Parcours de courses')
+        self.setGeometry(100, 100, 800, 600)
         
-        self.mainLayout = QStackedLayout()
-        self.label = QLabel("test")
-        self.label.setStyleSheet("background-color: rgba(0, 0, 0, 0);")
-        self.label.setAutoFillBackground(False)
-        self.label.setContentsMargins(0, 0, 0, 0)
-        self.label.setStyleSheet("background-color: rgba(0, 0, 0, 0);")
-        self.picture = QPixmap("./plan11.jpg")
+        # Layouts
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
+        self.layout_principal = QHBoxLayout(self.central_widget)       
+        self.dock_container = QVBoxLayout()
+        self.button_layout = QVBoxLayout()      
+        self.dock_layout = QVBoxLayout()
+        self.bottomRight_layout = QHBoxLayout()
         
-        self.pictureLabel = QLabel()
-        self.pictureLabel.setPixmap(self.picture)
-        self.pictureLabel.setStyleSheet("background-color: rgba(0, 0, 0, 0);")
+        # Widgets
+        self.analyse = QPushButton('Analyser')
+        self.analyse.setMaximumWidth(400)
+        self.ajout = QPushButton('Ajouter à la liste de courses')
+        self.supprimer = QPushButton('Supprimer de la liste de courses')
         
+        # Grid Widget
+        self.grid = Grid()
         
-        self.mainLayout.addWidget(self.scene)
-        self.mainLayout.addWidget(self.pictureLabel)
-        self.mainLayout.addWidget(self.label)
-        self.setLayout(self.mainLayout)
+        # Right Layout
+        self.rightLayout = QVBoxLayout()
+        self.rightLayout.addWidget(self.grid)
+        self.bottomRight_layout.addWidget(self.analyse)
+        self.rightLayout.addLayout(self.bottomRight_layout)
+
         
-        self.show()
+        # Add right layout to main layout
+        self.layout_principal.addLayout(self.dock_container)
+        self.layout_principal.addLayout(self.rightLayout)
         
+        # Menu bar
+        menu_bar = self.menuBar()
+        menu_fichier = menu_bar.addMenu("Fichier")
+        menu_fichier.addAction('Ouvrir', self.ouvrir)
+        
+        # Docks
+        self.dock = QDockWidget('Liste des articles')
+        self.addDockWidget(Qt.DockWidgetArea.LeftDockWidgetArea, self.dock)
+        self.articles_listWidget = QListWidget()
+        self.dock.setWidget(self.articles_listWidget)
+        self.dock.setMaximumWidth(300)
+        
+        self.dock2 = QDockWidget('Liste de course')
+        self.addDockWidget(Qt.DockWidgetArea.LeftDockWidgetArea, self.dock2)
+        self.liste_course = QListWidget()
+        self.dock2.setWidget(self.liste_course)
+        self.dock2.setMaximumWidth(300)
+        
+        # Add docks to dock layout
+        self.dock_layout.addWidget(self.dock)
+        self.dock_layout.addWidget(self.dock2)
+        
+        # Add buttons to button layout
+        self.button_layout.addWidget(self.ajout)
+        self.button_layout.addWidget(self.supprimer)
+        
+        # Add dock and button layouts to dock container
+        self.dock_container.addLayout(self.dock_layout)
+        self.dock_container.addLayout(self.button_layout)
+        
+        # Connect signals
+        self.ajout.clicked.connect(self.ajoutClicked.emit)
+        self.supprimer.clicked.connect(self.supprimerClicked.emit)
+        self.analyse.clicked.connect(self.analyseClicked.emit)
+
+    def ouvrir(self) -> str:
+        boite = QFileDialog()
+        chemin, validation = boite.getOpenFileName(directory=sys.path[0])
+        if validation:
+            self.fnameOpen.emit(chemin)
+            
+    
+    dico_articles = {'Légumes': 'carotte', "Légumes": 'tomate', "Hygiène": 'savon', "Hygiène": 'serviette'}
+    
+    def afficherArticles(self):
+        for categorie, produit in self.liste_articles:
+            # Ajouter la catégorie comme élément de haut niveau si elle n'existe pas déjà
+            if not any(categorie == self.articles_listWidget.item(i).text() for i in range(self.articles_listWidget.count())):
+                categorie_item = QListWidgetItem(categorie)
+                self.articles_listWidget.addItem(categorie_item)
+            # Ajouter le produit comme sous-élément
+            produit_item = QListWidgetItem(produit)
+            for i in range(self.articles_listWidget.count()):
+                if self.articles_listWidget.item(i).text() == categorie:
+                    self.articles_listWidget.item(i).addChild(produit_item)
+
+            
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    vue = vueGraphic()
+    fenetre = VueProjet()
+    fenetre.show()
     sys.exit(app.exec())
-    
