3475bac16eed808093f88ac15d58f349db0231e3
xMegumi <lorenzo.vdkn@gmail.com>
2024-06-03 17:18:19 +0200

    fail : missing one file

--- Controller.py
+++ Controller.py
@@ -85,11 +85,24 @@ class Controller:
         self.model.load(filename)
         self.view.load_window.hide()
         self.view.setCentralWidget(self.view.central_widget)
-        self.view.gridWidget.grid.setPicture(self.model.grille.getImage())
+        
+        width = self.model.grille.getTailleGrille()[0]
+        print(width)
+        height = self.model.grille.getTailleGrille()[1]
+        step = self.model.grille.getPas()
+        offset = self.model.grille.getDecalage()
+        lock = self.model.grille.getVerouiller()
+        positions : dict = self.model.getUsedCase()        
+        
+        self.view.gridWidget.grid.setPicture(self.model.grille.getImage()) # temporaire (il manque la mise à jour de la vue)
+        self.view.updateAllView(self.model.getArticlesCase(), self.model.currentCase, self.model.getCategoryJson, self.model.getCase(self.model.currentCase).getStatut(), self.model.getCase(self.model.currentCase).getCategory(),
+                                width, height, step, offset, lock, positions)
         
     def open_new_project(self, project):
         self.view.load_window.hide()
         self.view.setCentralWidget(self.view.central_widget)
+        
+        # manque la mise à jour de la vue
     
     def addCategory(self):
         list_category = ['aucune'] + list(self.model.getCategoryJson())
