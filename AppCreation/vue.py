fc5300ef53cc35840845bdbca30ffc05d9d1981a
xMegumi <lorenzo.vdkn@gmail.com>
2024-06-08 17:29:47 +0200

    ajout du nom de l'app (ShopRoute)

--- AppCreation/vue.py
+++ AppCreation/vue.py
@@ -305,6 +305,7 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setGeometry(100, 100, 800, 600)
+        self.setWindowTitle("ShopRoute")
         
         # Appliquer le style à l'application
         self.setStyleSheet(open("AppCreation/styles/qssSelect.qss").read())
